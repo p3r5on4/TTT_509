087585e80a8499b6a29656054916e481a356141b
p3r5on4 <103918607+p3r5on4@users.noreply.github.com>
2023-01-03 07:51:05 -0800

    Update test.py

--- test.py
+++ test.py
@@ -6,22 +6,12 @@ class Tests(unittest.TestCase):
     # o winner
     def test_get_winner_X(self):
         board = [
-            [None, None, 'X'],
-            ['O', 'O', 'O'],
-            [None, 'X', None],
+            ['X', None, 'O],
+            [None, 'O', None
+            ['O', 'X', None],
         ]
 
         self.assertEqual(logic._check_winners(board, 'X'), 'X')
-    
-    # Test a case where there is stalemate
-    def test_draw(self):
-        board = [
-            ['O', 'O', 'X'],
-            ['X', 'X', 'O'],
-            ['O', 'O', 'X'],
-        ]
-        
-        self.assertEqual(logic._check_winners(board, 'X'), None)
-
+            
 if __name__ == '__main__':
     unittest.main()
